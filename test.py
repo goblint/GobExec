d3d507dc4c52c794c2058dd00bf25a9497e4dde8
Simmo Saan <simmo.saan@gmail.com>
2022-07-05 16:37:49 +0300

    Combine test and table scripts

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import asyncio
 from pathlib import Path
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from gobexec.goblint import GoblintTool
 from gobexec.goblint.bench import txtindex
@@ -14,3 +15,11 @@ matrix = index.to_matrix(goblint)
 
 results = asyncio.run(matrix.execute_async())
 print(results)
+
+env = Environment(
+    loader=FileSystemLoader("./templates"),
+    autoescape=select_autoescape()
+)
+template = env.get_template("matrix.html")
+with open("out.html", "w") as outfile:
+    outfile.write(template.render(matrix=matrix,matrix_results=results))
