1614ed816deaf3ab73d9e54966a0f1c05dfa4372
Simmo Saan <simmo.saan@gmail.com>
2022-07-05 17:15:25 +0300

    Move templates to packages

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import asyncio
 from pathlib import Path
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+from jinja2 import Environment, select_autoescape, ChoiceLoader, PackageLoader
 
 from gobexec.goblint import GoblintTool
 from gobexec.goblint.bench import txtindex
@@ -14,10 +15,13 @@ index = txtindex.Index.from_path(Path("/home/simmo/dev/goblint/sv-comp/goblint-b
 matrix = index.to_matrix(goblint)
 
 env = Environment(
-    loader=FileSystemLoader("./templates"),
+    loader=ChoiceLoader([
+        PackageLoader("gobexec.output"),
+        PackageLoader("gobexec.output.html")
+    ]),
     autoescape=select_autoescape()
 )
-template = env.get_template("matrix.html")
+template = env.get_template("result.html")
 
 
 def render(results, progress=None):
